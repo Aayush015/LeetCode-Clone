78b4ed077fad5d6134ec736400dc033f3c0d22c8
Aayush Kafle <aayushkafle308@gmail.com>
2025-02-28 16:58:05 -0700

    Added Sign In button styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,9 +11,14 @@ const navbar:React.FC<navbarProps> = () => {
             <img src="/logo.png" alt="LeetClone" className="h-full"/>
         </Link>
         <div className="flex items-center">
-            <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
-            hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange">
-                 Sign In</button>
+        <button
+					className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
+                transition duration-300 ease-in-out
+                '>
+					Sign In
+				</button>
+
         </div>
     </div>
 }
