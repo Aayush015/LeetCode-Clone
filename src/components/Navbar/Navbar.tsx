d0572ea6e1f0b6f27e69079f471052ff0b76cfe7
Aayush Kafle <aayushkafle308@gmail.com>
2025-02-27 23:03:16 -0700

    Added Navbar and Button for auth

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 type navbarProps = {
@@ -5,7 +6,13 @@ type navbarProps = {
 };
 
 const navbar:React.FC<navbarProps> = () => {
-    
-    return <div>Have a good coding</div>
+    return <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+        <Link href="/" className="flex items-center justify-center h-20">
+            <img src="/logo.png" alt="LeetClone" className="h-full"/>
+        </Link>
+        <div className="flex items-center">
+            <button className="bg-brand-orange"> Sign In</button>
+        </div>
+    </div>
 }
 export default navbar;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
 
 type indexProps = {
@@ -6,6 +7,10 @@ type indexProps = {
 
 const index:React.FC<indexProps> = () => {
     
-    return <div>Auth Page</div>
+    return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+        <div className="max-w-7xl mx-auto">
+            <Navbar />
+        </div>
+    </div>
 }
 export default index;
