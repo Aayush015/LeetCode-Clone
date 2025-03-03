8686f53cc86eaac60d0e94f51fdde9763ddbe589
Aayush Kafle <aayushkafle308@gmail.com>
2025-03-02 17:27:15 -0700

    Added auth model with recoil to navigate through signin, forgot password, and create account

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,21 +1,25 @@
-import AuthModal from '@/components/Modals/AuthModal';
-import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+import { authModalState } from "@/atoms/authModalAtom";
+import AuthModal from "@/components/Modals/AuthModal";
+import Navbar from "@/components/Navbar/Navbar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+type AuthPageProps = {};
 
-type indexProps = {
-    
+const AuthPage: React.FC<AuthPageProps> = () => {
+	const authModal = useRecoilValue(authModalState);
+	return (
+		<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+			<div className='max-w-7xl mx-auto'>
+				<Navbar />
+				<div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+					<Image src='/hero.png' alt='Hero img' width={700} height={700} />
+				</div>
+				{authModal.isOpen && <AuthModal />}
+			</div>
+		</div>
+	);
 };
-
-const index:React.FC<indexProps> = () => {
-    
-    return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
-        <div className="max-w-7xl mx-auto">
-            <Navbar />
-            <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
-                <img src='\hero.png' alt='Hero img'/>
-            </div>
-            <AuthModal/>
-        </div>
-    </div>
-}
-export default index;
+export default AuthPage;
